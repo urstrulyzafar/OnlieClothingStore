5915be1738004b15d1d8a5ea45556d00245d1536
ZAFAR <ZAFAR@ZAFRULLAH>
2021-09-29 08:33:23 +0530

    create commit file

---
+++ OnlineClothingStore/Controllers/CustomerRegController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OnlineClothingStore.Models;
+
+namespace OnlineClothingStore.Controllers
+{
+    public class CustomerRegController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(CustomerModel cm)
+        {
+            string connection = "Data Source=ZAFRULLAH;Initial Catalog=dbMyOnlineShopping;Integrated Security=True";
+            using (SqlConnection sqlconn = new SqlConnection(connection))
+            {
+                string sqlquery = "insert into Customer (MemberId,FirstName,LastName,Email,Password,ContactNumber) values('" + cm.FirstName + "','" + cm.lastName + "','" + cm.Email + "','" + cm.Password + "','" + cm.ContactNumber + "') ";
+                using(SqlCommand sqlcomm=new SqlCommand(sqlquery, sqlconn))
+                {
+                    sqlconn.Open();
+                    sqlcomm.ExecuteNonQuery();
+                    ViewData["message"] = "The New Customer " + cm.FirstName + "is Saved Successfully";
+                }
+            }
+            return View(cm);
+        }
+    }
+}
---
+++ OnlineClothingStore/Models/CustomerModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace OnlineClothingStore.Models
+{
+    public class CustomerModel
+    { 
+        [Key]
+        public int MemberId { get; set; }
+
+        [Display(Name ="Firstname")]
+        [Required(ErrorMessage ="Please Enter Firstname")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Laststname")]
+        [Required(ErrorMessage = "Please Enter Lastname")]
+        public string lastName { get; set; }
+
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "Please Enter Email")]
+        public string Email { get; set; }
+
+        [Display(Name = "Password")]
+        [Required(ErrorMessage = "Please Enter Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Contact Number")]
+        [Required(ErrorMessage = "Please Enter Contact Number")]
+        public string ContactNumber { get; set; }
+
+        
+    }
+}
